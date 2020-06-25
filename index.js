dabb17d3b550e6477f0e95e16e9288bfbfa2a4dd
Alyson <ashaw2512@gmail.com>
2020-06-24 22:18:58 -0400

    Mapped arrays

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-let request = new XMLHttpRequest();
-request.open('GET', 'https://covidtracking.com/api/v1/us/current.json');
-
-request.onload = function () {
+function afterRequestLoads() {
+  
   const data = JSON.parse(this.response);
-  d3.select('#current-cases-text').html(`${d3.format(',')(data[0].positive)}`);
-  const date = data[0].date.toString();
+  d3.select('#current-cases-text').html(`${d3.format(',')(data[2].positive)}`);
+  const date = data[2].date.toString();
   const formattedDate = `${date.substring(4, 6)}/${date.substring(6, 8)}/${date.substring(0, 4)}`;
   d3.select('#current-cases-date').html(`<i>Updated: ${formattedDate}</i>`);
-};
 
+  let arrDate = data.map(function(element) {
+    return element.date;
+  });
+  console.log(arrDate);
+
+  let arrPositive = data.map(function(element) {
+    return element.positive;
+  });
+  console.log(arrPositive);
+}
+
+let request = new XMLHttpRequest();
+request.open('GET', 'https://covidtracking.com/api/v1/us/daily.json');
+request.onload = afterRequestLoads;
 request.send();
