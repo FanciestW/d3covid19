0e4ceb933e7761c1dd0569bfda6e3c8daa7964a7
William Lin <wlin26@yahoo.com>
2020-06-25 22:41:23 -0400

    Added sample line chart using chartist

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 function afterRequestLoads() {
-  
-  const data = JSON.parse(this.response);
-  d3.select('#current-cases-text').html(`${d3.format(',')(data[2].positive)}`);
-  const date = data[2].date.toString();
+  const covidData = JSON.parse(this.response);
+  d3.select('#current-cases-text').html(`${d3.format(',')(covidData[0].positive)}`);
+  const date = covidData[0].date.toString();
   const formattedDate = `${date.substring(4, 6)}/${date.substring(6, 8)}/${date.substring(0, 4)}`;
   d3.select('#current-cases-date').html(`<i>Updated: ${formattedDate}</i>`);
 
-  let arrDate = data.map(function(element) {
+  let arrDate = covidData.map(function (element) {
     return element.date;
   });
   console.log(arrDate);
 
-  let arrPositive = data.map(function(element) {
+  let arrPositive = covidData.map(function (element) {
     return element.positive;
   });
   console.log(arrPositive);
+
+  // Using Chartist.js
+  var data = {
+    // A labels array that can contain any sort of values
+    labels: covidData.map((e) => e.date),
+    // Our series array that contains series objects or in this case series data arrays
+    series: [covidData.map((e) => e.positiveIncrease)],
+  };
+
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object.
+  new Chartist.Line('.ct-chart', data);
 }
 
 let request = new XMLHttpRequest();
