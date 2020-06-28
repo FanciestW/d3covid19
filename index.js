bd5002f363b82189b894cb2f50ee5aef1d58eeb2
William Lin <wlin26@yahoo.com>
2020-06-27 21:00:56 -0400

    removed console log used for debugging

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 function graphLine(data) {
   let margin = { top: 10, right: 30, bottom: 30, left: 60 };
-  let width = 560 - margin.left - margin.right;
-  let height = 560 - margin.top - margin.bottom;
+  let width = window.innerWidth - margin.left - margin.right;
+  let height = 600 - margin.top - margin.bottom;
+
+  const numOfDataPoints = data.length;
+
+  let xScale = d3
+    .scaleLinear()
+    .domain([0, numOfDataPoints - 1])
+    .range([0, width]);
+  let yScale = d3
+    .scalePow()
+    .domain([0, Math.max(...data) + Math.min(...data)])
+    .range([height, 0]);
+
+  let line = d3
+    .line()
+    .x(function (d, i) {
+      return xScale(i);
+    })
+    .y(function (d) {
+      return yScale(d);
+    })
+    .curve(d3.curveMonotoneX);
 
   let svg = d3
     .select('.d3-line')
@@ -10,6 +31,30 @@ function graphLine(data) {
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  svg.append('g').attr('class', 'x-axis').attr('transform', `translate(0,${height})`).call(d3.axisBottom(xScale));
+  svg.append('g').attr('class', 'y-axis').call(d3.axisLeft(yScale));
+
+  svg.append('path').datum(data).attr('class', 'line').attr('d', line);
+
+  svg
+    .selectAll('.dot')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('cx', function (d, i) {
+      return xScale(i);
+    })
+    .attr('cy', function (d) {
+      return yScale(d);
+    })
+    .attr('r', 5)
+    .on('mouseover', function (a, b, c) {
+      console.log(a);
+      this.attr('class', 'focus');
+    })
+    .on('mouseout', function () {});
 }
 
 function afterRequestLoads() {
@@ -28,6 +73,8 @@ function afterRequestLoads() {
     return element.positive;
   });
   console.log(arrPositive);
+
+  graphLine(arrPositive.reverse());
 }
 
 let request = new XMLHttpRequest();
